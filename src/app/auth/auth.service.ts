b1027a1da8800fa01608dfbec7ba64c93db2dd2a
Tobias Toennishoff <t.toennishoff@tu-bs.de>
2019-12-07 12:38:36 +0100

    Using User Model to help with authentication

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,11 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+import { Plugins } from '@capacitor/core';
+
+import { User } from './user.model';
 
 export interface AuthResponseData {
   kind: string;
@@ -16,11 +21,27 @@ export interface AuthResponseData {
   providedIn: 'root'
 })
 export class AuthService {
-  private _userIsAuthenticated = false;
-  private _userId = null;
+  private _user = new BehaviorSubject<User>(null);
 
   get userIsAuthenticated() {
-    return this._userIsAuthenticated;
+    return this._user.asObservable().pipe(
+      map(user => {
+        if (user) {
+         return !!user.token;
+        } else {
+          return false;
+        }
+      }));
+  }
+
+  get userId() {
+    return this._user.asObservable().pipe(map(user => {
+      if (user) {
+        return user.id;
+      } else {
+        return null;
+      }
+    }));
   }
 
   constructor(private http: HttpClient) {}
@@ -29,16 +50,27 @@ export class AuthService {
     return this.http.post<AuthResponseData>(
       `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${environment.firebaseAPIKey}`,
       {email, password, returnSecureToken: true}
-    );
+    ).pipe(tap(this.setUserDate.bind(this)));
   }
 
   login(email: string, password: string) {
     return this.http.post<AuthResponseData>(
       `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.firebaseAPIKey}`,
-      {email, password});
+      {email, password}
+      ).pipe(tap(this.setUserDate.bind(this)));
   }
 
   logout() {
-    this._userIsAuthenticated = false;
+    this._user.next(null);
+  }
+
+  private setUserDate(userData: AuthResponseData) {
+    const expirationTime = new Date(new Date().getTime() + (+userData.expiresIn * 1000));
+    this._user.next(new User(userData.localId, userData.email, userData.idToken, expirationTime));
+  }
+
+  private storeAuthData(userId: string, token: string, tokenExpirationDate: string) {
+    const data = JSON.stringify({userId, token, tokenExpirationDate});
+    Plugins.Storage.set({key: 'authData', value: data});
   }
 }

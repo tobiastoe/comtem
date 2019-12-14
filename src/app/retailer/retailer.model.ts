53c6fd2429acd3c38e48296418c020ccbbd762e1
Tobias Toennishoff <t.toennishoff@tu-bs.de>
2019-12-14 12:49:46 +0100

    Created retailer model and retailer service

---
+++ src/app/retailer/retailer.model.ts
@@ -0,0 +1,9 @@
+export class Retailer {
+    constructor(
+        public id: string,
+        public name: string,
+        public email: string,
+        public address: string,
+        public imageUrl: string,
+    ) {}
+}

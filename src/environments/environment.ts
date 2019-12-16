e367f69004ff5e35c09e1a5125118975e5235223
Tobias Toennishoff <t.toennishoff@tu-bs.de>
2019-12-16 11:19:06 +0100

    Installed Firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,17 @@
 
 export const environment = {
   production: false,
-  firebaseAPIKey: 'AIzaSyB2uwayblBKpo0Hq7r1DeA8mXDj6FxoteA'
+  firebaseAPIKey: 'AIzaSyB2uwayblBKpo0Hq7r1DeA8mXDj6FxoteA',
+  firebaseConfig: {
+    apiKey: 'AIzaSyB2uwayblBKpo0Hq7r1DeA8mXDj6FxoteA',
+    authDomain: 'comtem-9282e.firebaseapp.com',
+    databaseURL: 'https://comtem-9282e.firebaseio.com',
+    projectId: 'comtem-9282e',
+    storageBucket: 'comtem-9282e.appspot.com',
+    messagingSenderId: '1096034586106',
+    appId: '1:1096034586106:web:000e76f5f4b1d352e86843',
+    measurementId: 'G-LZVB0XPGJR',
+  }
 };
 
 /*

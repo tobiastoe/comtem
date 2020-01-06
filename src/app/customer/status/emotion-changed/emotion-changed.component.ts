d5fea998b0a228b79dd3f34288dffc3f1be396f3
Tobias Toennishoff <t.toennishoff@tu-bs.de>
2020-01-06 14:22:03 +0100

    setting correct emojis for emotion-change

--- src/app/customer/status/emotion-changed/emotion-changed.component.ts
+++ src/app/customer/status/emotion-changed/emotion-changed.component.ts
@@ -8,12 +8,32 @@ import { ModalController } from '@ionic/angular';
 })
 export class EmotionChangedComponent implements OnInit {
   @Input() emotion: string;
+  imageUrl: string;
 
   constructor(
     private modalCtrl: ModalController,
   ) { }
 
   ngOnInit() {
+    switch (this.emotion) {
+      case 'Happy': {
+        this.imageUrl = 'http://pngimg.com/uploads/smiley/smiley_PNG79.png';
+        break;
+      }
+      case 'Relaxed': {
+        this.imageUrl = 'http://pngimg.com/uploads/smiley/smiley_PNG86.png';
+        break;
+      }
+      case 'Sad': {
+        this.imageUrl = 'http://pngimg.com/uploads/smiley/smiley_PNG113.png';
+        break;
+      }
+      case 'Stressed': {
+        this.imageUrl = 'http://pngimg.com/uploads/smiley/smiley_PNG124.png';
+        break;
+      }
+    }
+
     setTimeout(() => {
       this.onCancel();
       }, 2000);

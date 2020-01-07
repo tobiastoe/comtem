1f3d02b7c5c4e1bbab4fa51a558436d3dea076f3
Tobias Toennishoff <t.toennishoff@tu-bs.de>
2020-01-07 08:58:43 +0100

    Resetting Form and dismissing modal

--- src/app/admin/new-advice/new-advice.component.ts
+++ src/app/admin/new-advice/new-advice.component.ts
@@ -19,21 +19,17 @@ export class NewAdviceComponent implements OnInit {
     this.modalCtrl.dismiss(null, 'cancel');
   }
 
-  createNewAdvice() {
-
-    this.modalCtrl.dismiss({ message: 'New Advice has been created!'}, 'confirm');
-  }
-
   onSubmit(form: NgForm) {
     if (!form.valid) {
       return;
     }
-    const oldEmotion = form.value.oldEmotion;
-    const newEmotion = form.value.newEmotion;
+    const oldEmotion = form.value.oldEmotion.toLowerCase();
+    const newEmotion = form.value.newEmotion.toLowerCase();
     const description = form.value.description;
 
     console.log(oldEmotion, newEmotion, description);
-    this.createNewAdvice();
     form.reset();
+
+    this.modalCtrl.dismiss({ message: 'New Advice has been created!'}, 'confirm');
   }
 }
